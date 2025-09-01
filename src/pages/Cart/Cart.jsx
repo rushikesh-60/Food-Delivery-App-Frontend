5cc97dbac7352eecb455ce6057a0735df6a8dedd
rushikesh-60 <rushikeshsonparote9@gmail.com>
2025-09-01 13:46:28 +0530

    Cart page created

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, {  useContext } from 'react'
 import './Cart.css'
+import { StoreContext } from '../../context/StoreContext';
 
 const Cart = () => {
+
+  const {cartItems,food_list,removeFromCart,getTotalCartAmount} = useContext(StoreContext);
+
+  const totalAmount = getTotalCartAmount();
+  const deliveryAmount = 40;
+
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item,index)=>{
+          if(cartItems[item._id]>0){
+            return(
+              <div>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>Rs.{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>Rs.{cartItems[item._id] * item.price}</p>
+                  <p onClick={()=>{removeFromCart(item._id)}}  className='cross' >&minus;</p>
+                </div>
+                <hr />
+              </div>
+            )
+          }
+        })}
+      </div>
+
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>Rs.{totalAmount}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>Rs.{totalAmount>0?deliveryAmount:0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>Rs.{totalAmount>0? totalAmount+deliveryAmount:0}</b>
+            </div>
+          </div>
+
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        
+        <div className='cart-promocode'>
+          <div>
+            <p>If you have a promo code, Enter it here</p>
+            <div className='cart-promocode-input'>
+              <input type="text" placeholder='promo code'/>
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
