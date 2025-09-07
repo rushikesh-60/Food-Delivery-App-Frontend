1d51d9a5ff501259fe7a8adf5811743687a4dd39
rushikesh-60 <rushikeshsonparote9@gmail.com>
2025-09-07 15:05:05 +0530

    PlaceOrder page responsive

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -7,8 +7,14 @@ const AppDownload = () => {
     <div className='app-download' id='app-download'>
         <p>For Better Experience Download <br />Tomato App</p>
         <div className="app-download-platforms">
-            <img src={assets.play_store} alt="" />
-            <img src={assets.app_store} alt="" />
+            <a href="https://play.google.com/store" target="_blank" rel="noopener noreferrer">
+  <img src={assets.play_store} alt="Download on Play Store" />
+</a>
+
+<a href="https://www.apple.com/app-store/" target="_blank" rel="noopener noreferrer">
+  <img src={assets.app_store} alt="Download on App Store" />
+</a>
+
         </div>
     </div>
   )
